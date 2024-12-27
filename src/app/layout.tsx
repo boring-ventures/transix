50e76d50a2adb04d6a59ff53ebd3b5207d747343
BLVCKCENTURION <blvckcenturion@gmail.com>
2024-12-26 22:49:36 -0400

    feat: implemented auth provider on app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import "./globals.css"
 import { Providers } from '@/components/providers'
 import localFont from "next/font/local";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,7 +27,9 @@ export default function RootLayout({
         className={`${inter.variable} ${neutro.variable}`}
         suppressHydrationWarning
       >
-        <Providers>{children}</Providers>
+        <AuthProvider>
+          <Providers>{children}</Providers>
+        </AuthProvider>
       </body>
     </html>
   );
