e7f0596148840d6308f165c5396843a8061295c0
BLVCKCENTURION <blvckcenturion@gmail.com>
2024-12-26 22:49:26 -0400

    feat: / page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import { redirect } from 'next/navigation'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect('/login')
+export default async function Home() {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  redirect("/login");
 }
