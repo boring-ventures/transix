f47e364a309a7bc13d21c70762bd1ca67e12f069
BLVCKCENTURION <blvckcenturion@gmail.com>
2025-01-27 15:37:36 -0400

    feat: update to loading-table component

--- src/components/table/loading-table.tsx
+++ src/components/table/loading-table.tsx
@@ -9,7 +9,6 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Card, CardContent } from "@/components/ui/card";
 
 interface LoadingTableProps {
   columnCount?: number;
@@ -23,48 +22,59 @@ export function LoadingTable({
   showToolbar = true,
 }: LoadingTableProps) {
   return (
-    <Card>
-      <CardContent>
-        {showToolbar && (
-          <div className="flex items-center justify-between py-4">
-            <Skeleton className="h-9 w-[250px]" />
-            <div className="flex items-center space-x-2">
-              <Skeleton className="h-9 w-[100px]" />
-              <Skeleton className="h-9 w-9" />
-            </div>
+    <div className="space-y-4">
+      {showToolbar && (
+        <div className="flex items-center justify-between">
+          <div className="space-y-1">
+            <Skeleton className="h-7 w-[150px]" />
+            <Skeleton className="h-4 w-[250px]" />
           </div>
-        )}
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-9 w-[200px]" />
+            <Skeleton className="h-9 w-9" />
+          </div>
+        </div>
+      )}
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                {Array.from({ length: columnCount }).map((_, index) => (
-                  <TableHead key={index}>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {Array.from({ length: columnCount }).map((_, index) => (
+                <TableHead key={index} className="h-10">
+                  <Skeleton className="h-4 w-[100px]" />
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: rowCount }).map((_, rowIndex) => (
+              <TableRow key={rowIndex} className="h-16">
+                {Array.from({ length: columnCount }).map((_, colIndex) => (
+                  <TableCell key={colIndex}>
                     <Skeleton className="h-4 w-[100px]" />
-                  </TableHead>
+                  </TableCell>
                 ))}
               </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: rowCount }).map((_, rowIndex) => (
-                <TableRow key={rowIndex}>
-                  {Array.from({ length: columnCount }).map((_, colIndex) => (
-                    <TableCell key={colIndex}>
-                      <Skeleton className="h-4 w-[100px]" />
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
 
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Skeleton className="h-8 w-[100px]" />
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <Skeleton className="h-4 w-[150px]" />
           <Skeleton className="h-8 w-[70px]" />
+          <Skeleton className="h-4 w-[100px]" />
         </div>
-      </CardContent>
-    </Card>
+        <div className="flex items-center space-x-2">
+          <Skeleton className="h-8 w-8" />
+          <Skeleton className="h-8 w-8" />
+          <Skeleton className="h-4 w-[100px]" />
+          <Skeleton className="h-8 w-8" />
+          <Skeleton className="h-8 w-8" />
+        </div>
+      </div>
+    </div>
   );
 }
