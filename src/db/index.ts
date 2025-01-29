4a1c78c04d1ce071bb6bb56bf9051dcb50180829
BLVCKCENTURION <blvckcenturion@gmail.com>
2025-01-29 12:35:38 -0400

    feat: fix db index file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,12 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-
-const connectionString = process.env.DATABASE_URL!;
-
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './schema'
+// Make sure the environment variable is named correctly for Supabase
+const connectionString = process.env.NEXT_PUBLIC_DATABASE_URL!
+// Add SSL configuration for Supabase
 const client = postgres(connectionString, {
   prepare: false,
-  ssl: "require",
-});
+})
+const db = drizzle(client, { schema })
 
-export const db = drizzle(client, { schema });
+export { db }
